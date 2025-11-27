2d2b3daacf2a969f8295401ff4ff905cc2c3ea0a
FernandoMHaesbaert <fhaesbaert@gmail.com>
2025-11-27 01:50:04 -0300

    atualizando

---
+++ rascunhos.R
@@ -0,0 +1,11 @@
+# criar um data frame com duas variáveis
+df <- data.frame(
+  nome = c("Ana", "Bruno", "Carla", "Daniel"),
+  idade = c(23, 35, 29, 41)
+)
+<!-- escrever uma explicação sobre o exemplo: Exemplo em R – rede feed-forward para regressão com pacote nnet  -->
+A rede feed-forward é um tipo de rede neural artificial onde as conexões entre os nós não formam ciclos. Ela é amplamente utilizada para tarefas de regressão e classificação. No R, o pacote 'nnet' oferece uma maneira simples de implementar redes feed-forward.
+
+
+
+
